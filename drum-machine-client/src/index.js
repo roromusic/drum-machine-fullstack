00a4fe16f52fad652fc8d7d976ea2ca8b96d3eba
roromusic <roromusic@ucla.edu>
2018-02-17 18:34:03 -0800

    pre-preparation

--- drum-machine-client/src/index.js
+++ drum-machine-client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DrumMachine from './containers/DrumMachine';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<DrumMachine />, document.getElementById('root'));
 registerServiceWorker();
