0103519c436a2489dc108bb634d28b1263fc876b
roromusic <roromusic@ucla.edu>
2018-03-02 17:53:56 -0800

    add Control component

--- drum-machine-client/src/components/Controls.js
+++ drum-machine-client/src/components/Controls.js
@@ -2,37 +2,122 @@ import React, { Component } from 'react';
 import './Controls.css';
 
 class Controls extends Component {
-    
+    constructor(props){
+        super(props);
+        this.state = {
+            bpm: null
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+        this.incrementBPM = this.incrementBPM.bind(this);
+        this.decrementBPM = this.decrementBPM.bind(this);
+    }
+
+    updateStore(newData) {
+        this.props.updateBeat(newData);
+    }
+
+    handleChange(event) {
+        event.persist();
+        this.setState(prevState => {
+            return {
+                bpm: Number(event.target.value)
+            }
+        })
+    }
+
+    handleBlur() {
+        if(this.state.bpm < 60){
+            this.setState(() => ({bpm: 60}))
+            this.updateStore({bpm: 60})
+        } 
+        else if(this.state.bpm > 200){
+            this.setState(()=> ({bpm: 200}))
+            this.updateStore({bpm: 200})
+        } 
+        else this.updateStore({bpm: this.state.bpm})
+    }
+
+    incrementBPM(){
+        if(this.state.bpm === null){
+            this.setState(() => ({bpm: this.props.bpm === 200 ? 200 : this.props.bpm + 1}))
+            if(this.props.bpm < 200){
+                this.updateStore({bpm: this.props.bpm + 1})
+            }
+        }else {
+            this.setState(prevState => {
+                return {
+                    bpm: prevState.bpm === 200 ? 200 : prevState.bpm + 1
+                }
+            })
+            if(this.state.bpm < 200){
+                this.updateStore({bpm: this.state.bpm + 1})
+            }
+        }
+    }
+
+    decrementBPM(){
+        if(this.state.bpm === null){
+            this.setState(() => ({bpm: this.props.bpm === 60 ? 60 : this.props.bpm - 1}))
+            if(this.props.bpm > 60){
+                this.updateStore({bpm: this.props.bpm - 1})
+            }
+        }else {
+            this.setState(prevState => {
+                return {
+                    bpm: prevState.bpm === 60 ? 60 : prevState.bpm - 1
+                }
+            })
+            if(this.state.bpm > 60){
+                this.updateStore({bpm: this.state.bpm - 1})
+            }
+        }
+    }
+
     render() {
+        const {
+            bpm
+        } = this.props;
 
         return (
-            <div class="controls">
-                <div class="controls_left">
-                    <div class="play-stop">
-                        <button id="play" class="play-stop_button">
-                            <div class="play_button"></div>
+            <div className="controls">
+                <div className="controls_left">
+                    <div className="play-stop">
+                        <button id="play" className="play-stop_button">
+                            <div className="play_button"></div>
                         </button>
-                        <button id="stop" class="play-stop_button">
-                            <div class="stop_button"></div>
+                        <button id="stop" className="play-stop_button">
+                            <div className="stop_button"></div>
                         </button>
                     </div>
-                    <div class="metronome">
-                        <input class="metronome_checkbox" type="checkbox" checked />
+                    <div className="metronome">
+                        <input className="metronome_checkbox" type="checkbox" checked />
                         <label for="metronome_checkbox">metronome</label>
                     </div>
                 </div>
 
-                <div class="display">
-                    <div class="measure">
-                        <div class="measure_number">
-                            <div class="measure_measure">1</div>
-                            <div class="measure_beat">1</div>
+                <div className="display">
+                    <div className="measure">
+                        <div className="measure_number">
+                            <div className="measure_measure">1</div>
+                            <div className="measure_beat">1</div>
                         </div>
-                        <div class="measure_title">Measure</div>
+                        <div className="measure_title">Measure</div>
                     </div>
-                    <div class="bpm">
-                        <input class="bpm_input" type="number" min="60" max='200' value="60" />
-                        <label for="bpm_input">BPM</label>
+                    <div className="bpm">
+                        <div className="bpm_display">
+                            <input className="bpm_input" type="number" min="60" value={this.state.bpm || bpm} onChange={this.handleChange} onBlur={this.handleBlur}/>
+                            <label for="bpm_input">BPM</label>
+                        </div>
+                        <div className="bpm_change">
+                            <div className="bpm_plus" onClick={this.incrementBPM}>
+                                <span>+</span>
+                            </div>
+                            <div className="bpm_minus" onClick={this.decrementBPM}>
+                                <span>-</span>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
