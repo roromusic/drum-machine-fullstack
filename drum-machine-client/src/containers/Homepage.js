a1776093f34eeafb685e0131dae8a1a35d189723
roromusic <roromusic@ucla.edu>
2018-03-16 14:00:07 -0700

    Fix Drum Machine Interface

--- drum-machine-client/src/containers/Homepage.js
+++ drum-machine-client/src/containers/Homepage.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import * as actions from '../actions';
 import Interface from './Interface';
 import Header from '../components/Header';
+import './Homepage.css';
 
 class Homepage extends Component {
     constructor(props) {
