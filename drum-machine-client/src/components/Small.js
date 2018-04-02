a69192c3cf2cc51039701e0346a80ebfde4cb4d7
roromusic <roromusic@ucla.edu>
2018-04-01 17:40:02 -0700

    Responsive

---
+++ drum-machine-client/src/components/Small.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Small.css';
+
+const Small = () => {
+    return (
+        <div className="small">
+            <p className="small_p">Please use landscape mode</p>
+        </div>
+    )
+}
+
+export default Small;
--- drum-machine-client/src/containers/App.js
+++ drum-machine-client/src/containers/App.js
@@ -1,36 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import * as actions from '../actions';
 import Navbar from '../components/Navbar';
+import Small from '../components/Small';
 import Main from './Main';
 
-const App = ({
-  user,
-  handleSignIn,
-  handleLogOut,
-  editable,
-  handleSave,
-  saveStatus,
-  displayResult,
-  handleCreate,
-  handleDelete
-}) => (
-  <div>
-    <Navbar 
-      user={user}
-      onSignIn={handleSignIn}
-      onLogOut={handleLogOut}
-      editable={editable}
-      onSave={handleSave}
-      onCreate={handleCreate}
-      saveStatus={saveStatus}
-      displayResult={displayResult}
-      onDelete={handleDelete}
-    />
-    <Main />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isMobile: false
+    }
+
+    this.resize = this.resize.bind(this);
+  }
+
+  resize() {
+    this.setState(() => ({ isMobile: window.innerWidth <= 640}))
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
+  }
+
+  render() {
+    const {
+      user,
+      handleSignIn,
+      handleLogOut,
+      editable,
+      handleSave,
+      saveStatus,
+      displayResult,
+      handleCreate,
+      handleDelete
+    } = this.props;
+    console.log(this.state.isMobile);
+
+    return (
+      <div>
+        <Navbar
+          user={user}
+          onSignIn={handleSignIn}
+          onLogOut={handleLogOut}
+          editable={editable}
+          onSave={handleSave}
+          onCreate={handleCreate}
+          saveStatus={saveStatus}
+          displayResult={displayResult}
+          onDelete={handleDelete}
+        />
+        {
+          this.state.isMobile
+          ? <div>
+              <Small />
+            </div>
+          : <Main />
+        }
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   user: state.user,
